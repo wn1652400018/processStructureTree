84ef972650f82d15843a6dffe0fa7bd1cb3f8b3e
1652400018@qq.com <Administrator@PC-201802261901>
2018-06-01 13:33:17 +0800

    测试修改编码

--- src/edu/hust/structuretree/TxtFileFilter.java
+++ src/edu/hust/structuretree/TxtFileFilter.java
@@ -7,15 +7,17 @@ import javax.swing.filechooser.FileFilter;
 public class TxtFileFilter extends FileFilter {
 
 	@Override
+	
 	public boolean accept(File f) {
-		// TODO Auto-generated method stub
 		String fileName = f.getName();		
 		return f.isDirectory()||fileName.toLowerCase().endsWith(".txt");
 	}
 
 	@Override
+	/**
+	 * ÎÄ±¾¹ýÂËÆ÷
+	 */
 	public String getDescription() {
-		// TODO Auto-generated method stub
 		return "*.txt";
 	}
 
